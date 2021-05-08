badf2414939185c3493cd2003a966f18b4cb6eb5
Knha1 <60526919+Knha1@users.noreply.github.com>
2021-05-07 19:58:28 -0700

    Radio Button Start

--- demo.js
+++ demo.js
@@ -5,7 +5,9 @@ import {MyToolkit} from './mytoolkit.js';
 // Implement a MyToolkit Button
 var btn = new MyToolkit.Button();
 var check = new MyToolkit.Checkbox();
+var radio = new MyToolkit.RadioButton(3);
 
+check.move(100,200)
 btn.move(100,100);
 btn.onclick(function(e){
 	console.log(e);
--- mytoolkit.js
+++ mytoolkit.js
@@ -4,11 +4,12 @@ import {SVG} from './svg.min.js';
 //import { SVG } from '@svgdotjs/svg.js'
 
 var MyToolkit = (function() {
+    var draw = SVG().addTo('body').size('100%','100%');
     var Button = function(){
-        var draw = SVG().addTo('body').size('50%','50%');
+        
         var group = draw.group()
         var outline = group.rect(103,53).fill("black").radius(5)
-        var rect = group.rect(100,50).fill('#dbdbdb').radius(2)
+        var rect = group.rect(100,50).fill('#dbdbdb').radius(2).stroke({color: 'black', width:1, linecap: 'round'})
         var text = group.text("click me").move(23, 15.5)
 
         var clickEvent = null
@@ -53,19 +54,75 @@ var MyToolkit = (function() {
     }
 
     var Checkbox = function(){
-        var draw = SVG().addTo('body').size('100%','100%');
+        //var draw = SVG().addTo('body').size('100%','100%');
         var group = draw.group()
-        var outline = group.rect(30,30).fill("#636363").radius(5)
-        var rect = group.rect(28,28).fill("white").radius(5)
-        rect.move(rect.x() + 1, rect.y() + 1)
+        var rect = group.rect(28,28).fill("white").radius(5).stroke({color: 'black', width: 1})
+        var text = group.text("Checkbox").move(32, 7.5)
 
+        var line = group.line(14, 21, 22, 8).stroke({ color: 'white', width: 4, linecap: 'round' })
+        var line2 = group.line(8, 14, 14, 21).stroke({ color: 'white', width: 4, linecap: 'round' })
+        
+
+        
         group.click(function(event){
-            var check = draw.group()
-            var line = group.draw.line(0,100,100,0).move(20, 20)
-            line.stroke({color: 'black', width:10, linecap: 'round'})
+            if (line.attr().stroke == 'white'){
+                line.stroke({color: 'black'})
+                line2.stroke({color: 'black'})
+                }
+            else if (line.attr().stroke == 'black'){
+                line.stroke({color: 'white'})
+                line2.stroke({color: 'white'})
+                }
         })
+
+        return {
+            move: function(x, y) {
+                group.move(x,y)
+            },
+            onclick: function(eventHandler){
+                clickEvent = eventHandler
+            }
+        }
     }
-return {Button, Checkbox}
+
+    var RadioButton = function(n){
+        var group = draw.group()
+        // var rect = group.circle(24).fill("white").stroke({color: 'black', width: 1})
+        // var text = group.text("Radio Button " + String(n)).move(30, 6)
+        // var fill = group.circle(18).fill("black").move(3,3)
+        
+        
+        if (n > 2){
+            for (let i = 0; i < n; i ++){
+                console.log(i)
+                group.circle(24).fill("white").stroke({color: 'black', width: 1}). move(1 , 25 * i + 1)
+                group.circle(18).fill("black").move(4, 25 * i + 4)
+                group.text("Radio Button " + String(i + 1)).move(30 , 25 * i + 4)
+            }
+        }
+        else{
+            for (let i = 0; i < 2; i ++){
+                console.log(i)
+                group.circle(24).fill("white").stroke({color: 'black', width: 1}). move(1 , 25 * i + 1)
+                group.circle(18).fill("black").move(4, 25 * i + 4)
+                group.text("Radio Button " + String(i + 1)).move(30 , 25 * i + 4)
+            }
+        }
+        
+        group.click(function(event){
+
+        })
+
+        return {
+            move: function(x, y) {
+                group.move(x,y)
+            },
+            onclick: function(eventHandler){
+                clickEvent = eventHandler
+            }
+        }
+    }
+return {Button, Checkbox, RadioButton}
 }());
 
 export{MyToolkit}
